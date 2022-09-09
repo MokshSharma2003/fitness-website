e5a7df9188f0d6366b7ce2fbc82a226012d11139
MokshSharma2003 <109057221+MokshSharma2003@users.noreply.github.com>
2022-09-09 20:48:36 +0530

    backend

---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDeYiHBjxaPmAdyC-pm55D2-57ed7eB4us",
+    authDomain: "fit-india-2fd2a.firebaseapp.com",
+    projectId: "fit-india-2fd2a",
+    storageBucket: "fit-india-2fd2a.appspot.com",
+    messagingSenderId: "238604748237",
+    appId: "1:238604748237:web:53c277e715928328c73084"
+  };
+  
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
--- js/login.js
+++ js/login.js
@@ -1,17 +1,33 @@
 const loginText = document.querySelector(".title-text .login");
-      const loginForm = document.querySelector("form.login");
-      const loginBtn = document.querySelector("label.login");
-      const signupBtn = document.querySelector("label.signup");
-      const signupLink = document.querySelector("form .signup-link a");
-      signupBtn.onclick = (()=>{
-        loginForm.style.marginLeft = "-50%";
-        loginText.style.marginLeft = "-50%";
-      });
-      loginBtn.onclick = (()=>{
-        loginForm.style.marginLeft = "0%";
-        loginText.style.marginLeft = "0%";
-      });
-      signupLink.onclick = (()=>{
-        signupBtn.click();
-        return false;
-      });
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (() => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (() => {
+  signupBtn.click();
+  return false;
+});
+
+document.getElementById("loginform").addEventListener("submit",(event)=>{
+  event.preventDefault()
+})
+
+firebase.auth().onAuthStateChanged((user)=>{
+  if(user){
+    location.replace("welcome.html")
+  }
+})
+
+function login(){
+  const email =document.getElementById("email").value
+  const password = document.getElementById("password").value
+  firebase.auth().signInWithEmailAndPassword(email,password)
+}
